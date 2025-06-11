f065023b8e760d66757bfb7a666cbf25422e03e0
AnaVSSCaldeira <anavsscaldeira@gmail.com>
2025-06-11 19:28:29 -0300

    Aparicao dos poderes na tela

--- Salsicha/salsicha/Scenes/Game/Scripts/h_box_container.gd
+++ Salsicha/salsicha/Scenes/Game/Scripts/h_box_container.gd
@@ -3,6 +3,18 @@ extends HBoxContainer
 @onready var Card = preload("res://Scenes/Game/Scenes/power_card.tscn")
 
 func create_powers():
-    for i in range(3):
-        var card = Card.instantiate()
-        add_child(card)
+	var all_powers_list = $"/root/Global".powers
+	var powers_list = []
+	var unique_powers = $"/root/Global".unique_powers
+
+	for i in range(3):
+		var card = Card.instantiate()
+		var power = randi_range(1,6)
+		while power in powers_list or power in unique_powers:
+			power = randi_range(1,6)
+
+		powers_list.append(power)
+		card.get_node("Control").get_node("Name").text = all_powers_list[str(power)]["Name"]
+		card.get_node("Control").get_node("Description").text = all_powers_list[str(power)]["Description"]
+		
+		add_child(card)
--- Salsicha/salsicha/Scenes/global.gd
+++ Salsicha/salsicha/Scenes/global.gd
@@ -4,10 +4,33 @@ extends Node
 @export var bullet_type = "default"
 @export var bullet_damage = 1
 @export var powers_number = 3
-@export var powers = {"1": {"Name": "Calanguinho", "Description": "Vire um calanguinho! Diminua de tamanho e fique mais rápido!\nAperte a tecla Q para usar.\nEste poder substituirá o atual."}, "2": {"Name": "Projetil saboroso", "Description": "Atire cachorros-quentos nos inimigos! São mais lentos mas dão mais dano."}, "3": {"Name": "Dash Bolinha", "Description": "Ao correr, você vira uma bolinha e aumenta sua velocidade."}, "4": {"Name": "Aumento de velocidade", "Description": "Aumenta o valor da sua velocidade em 5%"}, "5": {"Name": "Aumento de ataque", "Description": "Aumenta o valor do seu ataque em 5%"}, "6": {"Name": "Latido Super Sônico", "Description": "Dê um latido poderoso que dá um bom dano em área mas sem muito alcance.\nAperte a tecla Q para usar.\nEste poder substituirá o atual."}}
+@export var powers = {
+    "1": {
+        "Name": "Calanguinho", "Description": "Vire um calanguinho! Diminua de tamanho e fique mais rápido!\nAperte a tecla Q para usar.\nEste poder substituirá o atual.", "Unique": true, "Active": true
+        }, 
+    "2": {
+        "Name": "Projetil saboroso", "Description": "Atire cachorros-quentos nos inimigos! São mais lentos mas dão mais dano.", "Unique": true, "Active": false
+        }, 
+    "3": {
+        "Name": "Dash Bolinha", "Description": "Ao correr, você vira uma bolinha e aumenta sua velocidade.", "Unique": true, "Active": false
+        }, 
+    "4": {
+        "Name": "Aumento de velocidade", "Description": "Aumenta o valor da sua velocidade em 5%", "Unique": false, "Active": false
+        }, 
+    "5": {
+        "Name": "Aumento de ataque", "Description": "Aumenta o valor do seu ataque em 5%", "Unique": false, "Active": false
+        }, 
+    "6": {
+        "Name": "Latido Super Sônico", "Description": "Dê um latido poderoso que dá um bom dano em área mas sem muito alcance.\nAperte a tecla Q para usar.\nEste poder substituirá o atual.", "Unique": true, "Active": true
+        }
+    }
+var unique_powers = []
+var current_ability = 0
 
 func restart():
     player_life = 5
     bullet_type = "default"
     bullet_damage = 1
     powers_number = 3
+    unique_powers = []
+    current_ability = 0
